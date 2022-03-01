eea2b5173e089c45d888f2555ef8a80b9c377aa2
rafaemilima <53321503+rafaemilima@users.noreply.github.com>
2022-03-01 20:25:55 -0300

    Add files via upload

--- src/validate.py
+++ src/validate.py
@@ -44,3 +44,51 @@ class Letter(Validation):
 class Alphanum(Validation):
     def validate(self):
         return self.string.isalpha() or self.string.isdigit()
+
+
+class Check(ABC):
+    def __init__(self, category):
+        self.category = category
+
+    @abstractmethod
+    def check(self):
+        pass
+
+
+class IsType(Check):
+    def check(self):
+        if self.category in ["RESE_INT", "RESE_VAZIO", "RESE_FLOAT", "RESE_STR", "RESE_BOOL", "RESE_CHAR"]:
+            return True
+        return False
+
+
+class IsConst(Check):
+    def check(self):
+        if self.category in ["CNST_INT", "CNST_FLOAT", "CNST_STR", "CNST_BOOL", "CNST_CHAR", "RESE_VERDADE",
+                             "RESE_FALSO"]:
+            return True
+
+        return False
+
+
+class IsEconcFirst(Check):
+    def check(self):
+        if self.category in ["ID", "OPE_ADI", "OPE_SUB", "DELI_OPAREN"] or IsConst(self.category).check():
+            return True
+        return False
+
+
+class IsCMD(Check):
+    def check(self):
+        if self.category in ["RESE_SE", "RESE_ENQUANTO", "RESE_LOOP", "RESE_PARE", "RESE_RETORNA", "RESE_ESCREVER",
+                             "RESE_ESCREVERPL", "RESE_LER"]:
+            return True
+        return False
+
+
+class IsRela(Check):
+    def check(self):
+        if self.category in ["OPE_MAIORQ", "OPE_MAIORI", "OPE_MENORI", "OPE_MENORQ", "OPE_IGUAL", "OPE_DIFE"]:
+            return True
+        else:
+            return False
